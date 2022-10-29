aef8c5a70d849fb6674c2f8555dba7a67db369c2
42decibel <hyukzuny@gmail.com>
2022-10-29 16:40:16 +0900

    feat: Set auth, board module

--- workspace/apps/api/src/app/app.module.ts
+++ workspace/apps/api/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [BoardModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
