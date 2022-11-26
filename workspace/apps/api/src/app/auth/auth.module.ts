2571737465012cbaf8cae4db8794fa7bc1d2056b
42decibel <hyukzuny@gmail.com>
2022-11-26 19:01:00 +0900

    feat: Add auth local strategy

--- workspace/apps/api/src/app/auth/auth.module.ts
+++ workspace/apps/api/src/app/auth/auth.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
-    PrismaModule
+    PrismaModule,
+    UserModule,
+    PassportModule
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
--- workspace/apps/api/src/app/auth/auth.service.ts
+++ workspace/apps/api/src/app/auth/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { V4 } from 'paseto';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private prisma: PrismaService
+        private prisma: PrismaService,
+        private userService: UserService
     ) {}
+
+    async validateUser(email: string, pass: string) {
+        const user = await this.userService.getUserByEmail(email);
+
+        if (user && user.password === pass) {
+            const { password, ...result } = user;
+
+            return result;
+        }
+
+        return null;
+    }
+
+    async signIn(req) {
+        return '';
+    }
 }
