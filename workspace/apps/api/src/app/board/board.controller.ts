b1be98828efa151b69e4eb5229909ca433268603
42decibel <hyukzuny@gmail.com>
2022-11-27 14:36:39 +0900

    feat: Init feed api

--- workspace/apps/api/src/app/board/board.controller.ts
+++ workspace/apps/api/src/app/board/board.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { Delete, Param, Post, Req } from '@nestjs/common/decorators';
+import { Controller, Get, UseInterceptors, UseFilters, Patch } from '@nestjs/common';
+import { Delete, Param, Post, Req, Body } from '@nestjs/common/decorators';
+import { UpdatePostDto } from '../common/dtos/create-post.dto';
+import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
+import { TransformInterceptor } from '../common/interceptors/transform.interceptor';
 import { BoardService } from './board.service';
 
 @Controller('board')
+@UseInterceptors(TransformInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class BoardController {
     constructor(
         private readonly boardSerive: BoardService
@@ -16,6 +21,11 @@ export class BoardController {
     @Delete(':id')
     deletePost(@Param() params) {
         return this.boardSerive.deletePost(params);
+    }
+
+    @Patch(':id')
+    updatePost(@Param() params, @Body() updatePostDto: UpdatePostDto) {
+        return this.boardSerive.updatePost(params, updatePostDto);
     }
 
     @Post()
--- workspace/apps/api/src/app/board/board.service.ts
+++ workspace/apps/api/src/app/board/board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -6,50 +6,72 @@ export class BoardService {
     constructor(private prisma: PrismaService) {}
 
     async getPostAll() {
-        return await this.prisma.post.findMany({
-            include: { user: true }
-        });
+        try {
+            const response = await this.prisma.post.findMany({
+                include: { user: true }
+            });
+    
+            return response;
+        }
+        catch(e) {
+            throw new NotFoundException();
+        }
     }
 
-    async updatePost(params, body) {
-        const id = params.id;
-        const updateData = body;
+    async updatePost(params, updatePostDto) {
+        try {
+            const id = params.id;
+            const updateData = updatePostDto;
 
-        const response = await this.prisma.post.update({
-            where: {
-                id: id
-            },
-            data: {
-            }
-        });
+            const response = await this.prisma.post.update({
+                where: {
+                    id: id
+                },
+                data: {
+                }
+            });
 
-        return response;
+            return response;
+        }
+        catch(e) {
+            throw new BadRequestException();
+        }
     }
 
     async deletePost(params) {
-        const id = params.id;
+        try {
+            const id = params.id;
 
-        const response = await this.prisma.post.delete({
-            where: {
-                id: id
-            }
-        });
+            const response = await this.prisma.post.delete({
+                where: {
+                    id: id
+                }
+            });
 
-        return response;
+            return response;
+        }
+        catch(e) {
+            throw new NotFoundException();
+        }
     }
 
     async createPost(body) {
-        const response = await this.prisma.post.create({
-            data: {
-                slug: body.slug,
-                title: body.title,
-                body: body.body,
-                published: body.published,
-                userId: body.userId
-            },
-            include: { user: true }
-        });
-
-        return response;
+        try {
+            const response = await this.prisma.post.create({
+                data: {
+                    slug: body.slug,
+                    title: body.title,
+                    body: body.body,
+                    published: body.published,
+                    userId: body.userId
+                },
+                include: { user: true }
+            });
+    
+            return response;
+        }
+        catch(e) {
+            throw new BadRequestException();
+        }
     }
 }
