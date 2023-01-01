23867b27fe2e59271b5066b002730d446f9dd62b
42decibel <hyukzuny@gmail.com>
2023-01-01 22:56:58 +0900

    fix: Fix create model parameter

--- workspace/apps/api/src/app/common/dtos/wiki.dto.ts
+++ workspace/apps/api/src/app/common/dtos/wiki.dto.ts
@@ -2,12 +2,9 @@ import { IsNotEmpty, IsString, IsNumber, IsBoolean } from "class-validator";
 
 export class CreateWikiDto {
     @IsNotEmpty()
-    slug: string;
+    topic: string;
 
     @IsNotEmpty()
-    @IsString()
-    title: string;
-
     @IsString()
     body: string;
 
